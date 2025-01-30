30db0bf2124d02757a930ba867dd0e5f2a99e873
22051815 <22051815@kiit.ac.in>
2025-01-31 01:11:07 +0530

    Gemini model Updated

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def get_conversation_chain():
 
     Answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.4)
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.4)
     prompt=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain=load_qa_chain(model,chain_type="stuff",prompt=prompt)
     return chain
